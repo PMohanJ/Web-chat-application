56dd03221a3a468385ec24b9fa72ca9a878b78e2
pmohanj <pmohan6712@gmail.com>
2022-11-16 22:11:45 +0530

    Added context for state management

---
+++ frontend/src/context/ChatProvider.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({childern}) => {
+    const [user, setUser] = useState();
+    const navigate = useNavigate();
+
+    // get the user state of login
+    useEffect(() => {
+        // we still yet to provide jwt functionailty...
+        const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        setUser(userInfo);
+
+        if (!userInfo){
+            navigate("/");
+        }
+    }, [history]);
+    return(
+        <ChatContext.Provider value={{user,setUser}}>
+          {childern}
+        </ChatContext.Provider>
+    ) 
+};
+
+export const ChatState = () => {
+    return useContext(ChatContext);
+}
+
+export default ChatProvider;
