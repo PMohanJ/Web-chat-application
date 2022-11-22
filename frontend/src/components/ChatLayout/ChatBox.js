2124c40d2f302dfe894b6e8c6bda919250dd8110
pmohanj <pmohan6712@gmail.com>
2022-11-22 20:36:20 +0530

    Implemented UI comp for Chat and chatBox, and api call to /api/chat/:userId router

--- frontend/src/components/ChatLayout/ChatBox.js
+++ frontend/src/components/ChatLayout/ChatBox.js
@@ -1,8 +1,69 @@
 import React from 'react'
+import { Box, Text } from '@chakra-ui/react'
+import { ChatState } from '../../context/ChatProvider';
 
 const ChatBox = () => {
+  const {selectedChat, setSelectedChat, user, chats, setChats} = ChatState();
+
+  function getSenderName(chat) {
+    if (chat.users[0]._id === user._id) 
+      return chat.users[1].name;
+    else 
+      return chat.users[0].name;
+  }
+  
+
   return (
-    <div>ChatBox</div>
+    <>
+      {selectedChat ? 
+        <Box
+          display={{base: selectedChat? "none" : "flex", md: "flex"}}
+          flexDir="column"
+          bg="white"
+          w={{base: "100%", md: "70%"}}
+          borderRadius="10px"
+          borderWidth="1px"
+          p="5px 10px 5px 10px"
+          m="5px"
+        >
+          {selectedChat &&
+            <Box
+              display="flex"
+              flexDir="row"
+              justifyContent="space-between"
+              p="10px"
+              bg="gray.300"
+              borderRadius="xl"
+            >
+              <Text fontSize="2xl">
+                {selectedChat.isGroupChat? selectedChat.chatName: getSenderName(selectedChat)}
+              </Text>
+            </Box>  
+          }
+          <Box
+            display="flex"
+            flexDir="column"
+            bg="gray.200"
+            h="100%"
+          >
+            
+          </Box>
+        </Box>
+        : <Box
+            display={{base: selectedChat? "none" : "flex", md: "flex"}}
+            flexDir="column"
+            bg="white"
+            w={{base: "100%", md: "70%"}}
+            borderRadius="10px"
+            borderWidth="1px"
+            p="5px"
+            m="5px"
+          >
+            <Text display="flex" fontSize="3xl" justifyContent="center">Please select a user to chat</Text>
+          </Box>
+      }
+    </>
+    
   )
 }
 
--- frontend/src/components/ChatLayout/MyChats.js
+++ frontend/src/components/ChatLayout/MyChats.js
@@ -1,5 +1,6 @@
-import { useToast } from '@chakra-ui/react';
-import React from 'react'
+import { useToast, Box, Text, Button, Stack} from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons'
+import React, { useEffect } from 'react'
 import { ChatState } from '../../context/ChatProvider';
 import { useState } from 'react';
 import axios from "axios"
@@ -11,12 +12,15 @@ const MyChats = () => {
 
   const fetchChats = async() => {
     try {
-      const { data } = await axios.get("http://localhost:8000/api/chat/", 
-      {
-        headers: {
-          "Content-Type":"application/json"
+      const url = "http://localhost:8000/api/chat/"+ user._id
+      const { data } = await axios.get(url, 
+        {
+          headers: {
+            "Content-Type":"application/json"
+          }
         }
-      })
+      )
+      console.log(data)
       setChats(data);
     } catch (error) {
         toast({
@@ -27,11 +31,73 @@ const MyChats = () => {
         });
     }
   }
+
+  useEffect(() => {
+    fetchChats();
+  },[])
+
+  function getSenderName(chat) {
+    if (chat.users[0]._id === user._id) 
+      return chat.users[1].name;
+    else 
+      return chat.users[0].name;
+  }
   
   return (
-    <div>
-      MyChats
-    </div>
+    <Box
+      display={{base: selectedChat? "none" : "flex", md: "flex"}}
+      flexDir="column"
+      bg="white"
+      w={{base: "100%", md: "30%"}}
+      borderRadius="10px"
+      borderWidth="1px"
+      p="5px"
+      m="5px"
+    >
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        fontSize={"3xl"}
+        fontFamily="-moz-initial"
+
+      >
+        <Text>Chats</Text>
+        <Button
+          display="flex"
+          rightIcon={<AddIcon/>}
+          fontFamily="-moz-initial"
+          fontWeight="semibold"
+          >
+           Create a Group
+            
+          </Button>
+      </Box>
+
+      {chats? (
+        <Stack >
+            {chats.map((chat) => (
+              <Box
+                bg={selectedChat === chat ? "#62c8f0": "gray.400"}
+                color={selectedChat === chat ? "black": "black"}
+                key={chat._id}
+                borderRadius="5px"
+                p="7px"
+                onClick={() => setSelectedChat(chat)}
+              >
+                <Text fontSize="large">
+                  {chat.isGroupChat? chat.chatName: getSenderName(chat)}
+                </Text>
+                <Text fontSize="small">
+                  {chat.latestMessage === "000000000000000000000000" ? "Yo bro no msg YET!": chat.latestMessage.content}
+                </Text>
+              </Box>
+            ))}
+        </Stack>
+      ): <Box >
+            <Text>Please add some friends to chat with</Text>
+         </Box>
+        }
+    </Box>
   )
 }
 
