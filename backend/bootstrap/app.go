6d4977bf38830963242d1107e9156531a7d9f636
pmohanj <pmohan6712@gmail.com>
2023-04-11 12:43:35 +0530

    Facilitate to use env varaible for testing at github actions env

--- backend/bootstrap/app.go
+++ backend/bootstrap/app.go
@@ -1,6 +1,10 @@
 package bootstrap
 
-import "github.com/pmohanj/web-chat-app/mongo"
+import (
+	"os"
+
+	"github.com/pmohanj/web-chat-app/mongo"
+)
 
 type Application struct {
 	Env   *Env
@@ -10,7 +14,14 @@ type Application struct {
 func App() Application {
 	app := &Application{}
 	app.Env = NewEnv()
-	app.Mongo = DBinstance(app.Env.MongoDBURL)
+
+	// To facilitate for testing env in github actions
+	MongoDBURL := os.Getenv("MongoDBURL")
+	if MongoDBURL == "" {
+		app.Mongo = DBinstance(app.Env.MongoDBURL)
+	} else {
+		app.Mongo = DBinstance(MongoDBURL)
+	}
 
 	return *app
 }
